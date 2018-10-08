348041ddf146a196ae0aa2aac8798065fcf8c4e6
Allega <42183540+MrsMars@users.noreply.github.com>
2018-10-08 11:59:10 +0300

    progress bar in the about page

--- m4/JS/control.js
+++ m4/JS/control.js
@@ -1,7 +1,16 @@
 $(function(){
     "use strict";
     
+    
+    // contacts - every page
     $('#info').click(function(){
         $('#info_box').toggleClass('open');
     }); 
+    
+    // progress - about page
+    $('.pr_bar').each(function(){
+        let width = $(this).data('skill');
+        $(this).css('width', width + '%');
+    });
+    
 });
